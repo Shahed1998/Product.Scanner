3bc1d7336c8d6f625bb20dbbffca82d3daad6d7c
Shahed1998 <shahedchowdhury120@gmail.com>
2023-03-07 01:24:55 +0600

    initial

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Product.Scanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product.Scanner.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<ProductModel> Products { get; set; }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Product.Scanner.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Details { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public bool IsUsed { get; set; } = false;
+        [Required]
+        public string QRCode { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
